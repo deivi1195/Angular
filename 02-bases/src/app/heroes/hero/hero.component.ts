7635e4b0733f8d160cd968e8f47be32146bde430
Deivi <deivig1195@gmail.com>
2025-01-19 15:37:58 -0400

    interpolacion, estructura HTML y estilos

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -3,10 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-hero',
   standalone: false,
-
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+  public name: string = 'ironman';
+  public age: number = 45;
+
 
 }
