1fa2fe15bba6d8874d9cd2d8b3ffb6c5855894a9
Deivi <deivig1195@gmail.com>
2025-01-20 10:10:19 -0400

    modulos y componentes arreglados

--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dbz-main-page',
+  selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   standalone: false
 })
