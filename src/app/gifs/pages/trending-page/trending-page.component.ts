74dc53d7aec78e2aea2c56b8cd5e817183028692
Deivi Gonzalez <deivig1195@gmail.com>
2025-06-21 02:02:42 -0400

    Mostrar gifs en pantalla - diseno de pantalla (buscador de gifs) - mostrar resultado de busqueda

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { GifListComponent } from '../../components/gif-list/gif-list.component';
 import { GifService } from '../../services/gifs.service';
 
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,6 +4,7 @@ import { environment } from '@environments/environment.development';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import type { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
+import { map, tap } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
@@ -12,6 +13,7 @@ export class GifService {
 
   trendingGifs = signal<Gif[]>([]);
   trendingGifsLoading = signal<boolean>(true);
+
 
   constructor() {
 
@@ -34,6 +36,31 @@ export class GifService {
       console.log({ gifs });
 
     });
+
+  }
+
+  searchGifs( query: string) {
+
+    return this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+      params: {
+        api_key: environment.giphyApiKey,
+        limit: 20,
+        q: query,
+      }
+    })
+    .pipe(
+      map ( ({ data }) => data ),
+      map ( ( items ) => GifMapper.mapGiphyItemsToGifArray(items) ),
+
+      //TODO: Historial
+    )
+    // .subscribe((resp) => {
+
+    //   const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+    //   console.log({ gifs });
+
+    // });
+
 
   }
 
