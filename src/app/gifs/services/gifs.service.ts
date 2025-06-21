dc7fb0003b8a4b5010b73ffd50d6d52f6b487878
Deivi Gonzalez <deivig1195@gmail.com>
2025-06-20 22:44:19 -0400

    Primer commit del dia 6/20/25 - mostrar gifs en pantalla

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '@environments/environment.development';
+import type { GiphyResponse } from '../interfaces/giphy.interfaces';
+import type { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../mapper/gif.mapper';
+
+@Injectable({providedIn: 'root'})
+export class GifService {
+
+  private http = inject(HttpClient);
+
+  trendingGifs = signal<Gif[]>([]);
+  trendingGifsLoading = signal<boolean>(true);
+
+  constructor() {
+
+    this.loadTrendingGifs();
+    console.log('Servicio creado');
+
+  }
+
+  loadTrendingGifs() {
+
+    this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
+      params: {
+        api_key: environment.giphyApiKey
+      }
+    }).subscribe((resp) => {
+
+      const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+      this.trendingGifs.set(gifs);
+      this.trendingGifsLoading.set(false);
+      console.log({ gifs });
+
+    });
+
+  }
+
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+  // ApiKeys
+  giphyApiKey: 'rYBGqdTBTmhuoHlpdSaJxvNGUy1OPpFr',
+  giphyUrl: 'https://api.giphy.com/v1',
+
+  //URLS
+
+};
